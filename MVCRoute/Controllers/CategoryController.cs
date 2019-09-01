a6bc5dfb6237a696b6e4a8b9f3d82531548e19f9
muhsin reyhan <muhsinreyhan@hotmail.com>
2019-09-01 14:21:52 +0300

    Add project files.

---
+++ MVCRoute/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using MVCRoute.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRoute.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Category
+
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public ActionResult Details(string CategoryName, int CategoryID, string ProductName, int ProductID)
+        {
+            return View();
+        }
+    }
+}
